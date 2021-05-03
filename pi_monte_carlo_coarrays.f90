2133d2dba95ba3bcbc147987bdda032170f66466
vmagnin <vincent.magnin@libertysurf.fr>
2021-05-03 15:19:43 +0200

    New version

--- pi_monte_carlo_coarrays.f90
+++ pi_monte_carlo_coarrays.f90
@@ -1,40 +1,39 @@
-! Computes Pi with a Monte Carlo algorithm
+! Computes an approximation of Pi with a Monte Carlo algorithm
 ! Coarrays version
-! Vincent  Magnin, 2021-04-22
+! Vincent Magnin, 2021-04-22
+! Last modification: 2021-05-02
 ! MIT license
-! $ caf -Wall -Wextra -std=f2008 -pedantic -O3 pi_monte_carlo_coarrays.f90 && time cafrun -n 2 ./a.out
-
+! $ caf -Wall -Wextra -std=f2008 -pedantic -O3 pi_monte_carlo_coarrays.f90 && time cafrun -n 4 ./a.out
+! or with ifort :
+! $ ifort -O3 -coarray pi_monte_carlo_coarrays_v2.f90 && time ./a.out
 
 program pi_monte_carlo_coarrays
     use, intrinsic :: iso_fortran_env, only: wp=>real64, int64
     implicit none
-    real(wp)        :: x, y     ! Coordinates
+    real(wp)        :: x, y     ! Coordinates of a point
     integer(int64)  :: n        ! Total number of points
-    integer(int64)  :: k[*]     ! Points into the circle
-    integer(int64)  :: i, j, kt, it
-    integer(int64)  :: n_im
+    integer(int64)  :: k[*]     ! Points into the quarter disk
+    integer(int64)  :: i        ! Loop counter
+    integer(int64)  :: j, kt, it
+    integer(int64)  :: n_per_image     ! Number of parallel images
 
     n = 1000000000
     k = 0
 
     print '(i2, a, i2, a)', this_image(), "/", num_images(), " images"
-    n_im = n / num_images()
-    print '(a, i11, a)', "Each image will compute", n_im, " points"
+    n_per_image = n / num_images()
+    print '(a, i11, a)', "I will compute", n_per_image, " points"
 
-    do i = 1, n_im
-        ! Computing a random point (x,y) into the square:
+    do i = 1, n_per_image
+        ! Computing a random point (x,y) into the square 0<=x<1, 0<=y<1:
         call random_number(x)
-        x = 2.0_wp * x - 1.0_wp
         call random_number(y)
-        y = 2.0_wp * y - 1.0_wp
 
-        ! Is it in the circle ?
-        if ((x**2 + y**2) <= 1.0_wp) then
-            k = k + 1    ! Yes
-        end if
+        ! Is it in the quarter disk (R=1, center=origin) ?
+        if ((x**2 + y**2) < 1.0_wp) k = k + 1
 
         ! Once in a while:
-        if (mod(i, n_im/40) == 0) then
+        if (mod(i, n_per_image/20) == 0) then
             sync all
             if (this_image() == 1) then
                 kt = 0
--- pi_monte_carlo_serial.f90
+++ pi_monte_carlo_serial.f90
@@ -1,37 +1,35 @@
-! Computes Pi with a Monte Carlo algorithm
+! Computes an approximation of Pi with a Monte Carlo algorithm
 ! Serial version
-! Vincent  Magnin, 2021-04-22
+! Vincent Magnin, 2021-04-22
+! Last modification: 2021-05-02
 ! MIT license
 ! $ gfortran -Wall -Wextra -std=f2008 -pedantic -O3 pi_monte_carlo_serial.f90 && time ./a.out
 
 program pi_monte_carlo_serial
     use, intrinsic :: iso_fortran_env, only: wp=>real64, int64
     implicit none
-    real(wp)        :: x, y     ! Coordinates
+    real(wp)        :: x, y     ! Coordinates of a point
     integer(int64)  :: n        ! Total number of points
-    integer(int64)  :: k = 0    ! Points into the circle
-    integer(int64)  :: i
+    integer(int64)  :: k = 0    ! Points into the quarter disk
+    integer(int64)  :: i        ! Loop counter
 
     n = 1000000000
 
-    do i = 1, n, +1
-        ! Computing a random point (x,y) into the square:
+    do i = 1, n
+        ! Computing a random point (x,y) into the square 0<=x<1, 0<=y<1:
         call random_number(x)
-        x = 2.0_wp * x - 1.0_wp
         call random_number(y)
-        y = 2.0_wp * y - 1.0_wp
 
-        ! Is it in the circle ?
-        if ((x**2 + y**2) <= 1.0_wp) then
-            k = k + 1    ! Yes
-        end if
+        ! Is it in the quarter disk (R=1, center=origin) ?
+        if ((x**2 + y**2) < 1.0_wp) k = k + 1
 
-        ! Once in a while:
-        if (mod(i, n/40) == 0) then 
+        ! Print the approximation once in a while:
+        if (mod(i, n/20) == 0) then
             write(*, '(i12, 4x, i12, 4x, f17.15)') i, k, (4.0_wp * k) / i
         end if
     end do
 
-    write(*, '(a, f17.15)') "Pi ~ ", (4.0_wp*k) / n
+    write(*,*)
+    write(*, '(a, f17.15)') "Pi ~ ", (4.0_wp * k) / n
 
 end program pi_monte_carlo_serial
