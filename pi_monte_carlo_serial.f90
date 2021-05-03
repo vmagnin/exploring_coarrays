482f5036439ccdf3bb17083b452eb76e75edb699
vmagnin <vincent.magnin@libertysurf.fr>
2021-05-03 18:56:09 +0200

    pi_monte_carlo_serial.f90: computes result only after the loop

--- pi_monte_carlo_serial.f90
+++ pi_monte_carlo_serial.f90
@@ -1,9 +1,10 @@
 ! Computes an approximation of Pi with a Monte Carlo algorithm
 ! Serial version
 ! Vincent Magnin, 2021-04-22
-! Last modification: 2021-05-02
+! Last modification: 2021-05-03
 ! MIT license
 ! $ gfortran -Wall -Wextra -std=f2008 -pedantic -O3 pi_monte_carlo_serial.f90 && time ./a.out
+! $ ifort -O3 pi_monte_carlo_serial.f90 && time ./a.out
 
 program pi_monte_carlo_serial
     use, intrinsic :: iso_fortran_env, only: wp=>real64, int64
@@ -22,14 +23,10 @@ program pi_monte_carlo_serial
 
         ! Is it in the quarter disk (R=1, center=origin) ?
         if ((x**2 + y**2) < 1.0_wp) k = k + 1
-
-        ! Print the approximation once in a while:
-        if (mod(i, n/20) == 0) then
-            write(*, '(i12, 4x, i12, 4x, f17.15)') i, k, (4.0_wp * k) / i
-        end if
     end do
 
     write(*,*)
+    write(*, '(a, i0, a, i0)') "4 * ", k, " / ", n
     write(*, '(a, f17.15)') "Pi ~ ", (4.0_wp * k) / n
 
 end program pi_monte_carlo_serial
